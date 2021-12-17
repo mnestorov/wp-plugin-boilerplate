f15b28957f45b725b5c37d303f001d7605c09ddc
Martin Nestorov <mbnestorov@gmail.com>
2021-12-17 17:32:23 +0200

    Comments inconsistentcy fix

--- dx-plugin-name/includes/classes/class-plugin-name-register-post-types.php
+++ dx-plugin-name/includes/classes/class-plugin-name-register-post-types.php
@@ -14,16 +14,14 @@
  */
 class Plugin_Name_Register_Post_Types {
     /**
-     * Register the `Business` post type
+     * Register the `Business` post type.
      *
      * @return void
      */
     public function register_business_type() {
-
         /**
          * Post Type: Businesses
          */
-
         $labels = array(
             'name'                  => __('Businesses', 'load_plugin_textdomain'),
             'singular_name'         => __('Business', 'load_plugin_textdomain'),
--- dx-plugin-name/includes/classes/class-plugin-name-register-taxonomies.php
+++ dx-plugin-name/includes/classes/class-plugin-name-register-taxonomies.php
@@ -14,16 +14,14 @@
  */
 class Plugin_Name_Register_Taxonomies {
     /**
-     * Register the `Size` taxonomy
+     * Register the `Size` taxonomy.
      *
      * @return void
      */
     public function register_size_taxonomy() {
-
         /**
          * Taxonomy: Sizes
          */
-
         $labels = array(
             'name'                  => __('Sizes', 'load_plugin_textdomain'),
             'singular_name'         => __('Size', 'load_plugin_textdomain'),
@@ -63,14 +61,13 @@ class Plugin_Name_Register_Taxonomies {
     }
 
     /**
-     * Register `location` taxonomy
+     * Register `location` taxonomy.
      *
      * @return void
     */
     public function register_location_taxonomy() {
-
         /**
-         * Taxonomy: Sizes
+         * Taxonomy: Location
          */
         $labels = array(
             'name'                  => __('Locations', 'load_plugin_textdomain'),
